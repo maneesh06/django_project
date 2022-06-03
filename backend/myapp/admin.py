e28b3865621f4d9a79523c77993c67a6292e6a26
PRASHANT SINGH <singhprashant070800@gmail.com>
2022-06-03 23:34:14 +0530

    more better

--- backend/myapp/admin.py
+++ backend/myapp/admin.py
@@ -5,11 +5,12 @@ from .models import Person,PersonVisit
 class PersonVisitInline(admin.TabularInline):
     model = PersonVisit
     list_display = ("person","captured_onn","id")
-    fields = ("person","captured_onn","id")
+    readonly_fields = ("person","captured_onn","id")
 @admin.register(Person)
 class PersonAdmin(admin.ModelAdmin):
     inlines = [PersonVisitInline]
-    list_display= ("first_name","last_name","id")
-    fields = ("first_name","last_name")
+    list_display= ("first_name","last_name","id","captured_on")
+    # fields = ("first_name","last_name")
+    readonly_fields=("first_name","last_name","id","captured_on")
 
 # Register your models here.
